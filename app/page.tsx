de324fdda9c97b5cdcbd69552bf10823854ec32e
Iqbal Hussain Syed <65956129+IHSyed@users.noreply.github.com>
2023-03-01 15:52:57 +0500

    Second Commit. Amended h3 tag

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
     <div className='mx-4 self-center text-center'>
       <h1 className='text-6x1 font-bold text-blue-700'>Welcome to Panaverse Dao Community</h1>
       <h2 className='text-3x1 font-semibold text-blue-500'>A Community of Web 3 and Metaverse Developers</h2>
-      <h3> Project by Iqbal Hussain Syed # 1</h3>
+      <h3> Project by Iqbal Hussain Syed </h3>
       <button className='my-4 px-4 py-2 border-2 border-black rounded-lg text-white bg-blue-900'>Learn More</button>
       </div>
       </div>
